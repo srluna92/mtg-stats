270242163df0495d6ddc75da54f810cb238ae6c1
sluna <samuel.luna@cabelas.com>
2018-04-17 15:01:20 -0500

    update

--- src/app/deck/decklist/decklist.component.ts
+++ src/app/deck/decklist/decklist.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { FireService } from '../../services/fire.service';
 import { CardService } from '../../services/card.service';
 import { Card } from '../../models';
+import { forEach } from '@firebase/util';
 
 @Component({
   selector: 'app-decklist',
@@ -16,15 +17,37 @@ export class DecklistComponent implements OnInit {
   sortDeck(): void {
     this.deckList = this.deckList.sort((a, b) => {
       if (a.main === b.main) {
-        return a[this.sort] < b[this.sort] ? -1 : 1;
-      } else if (a.main && !b.main) {
-        return 1;
-      } else if (!a.main && b.main) {
-        return -1;
+        switch (this.sort) {
+          // case 'colors': return this.compareColors(a, b);
+          default: return a[this.sort] < b[this.sort] ? 1 : -1;
+        }
+      } else {
+        return a.main ? 1 : -1;
       }
     });
   }
-
+  compareColors(a: Card, b: Card) {
+    if (a.colors.length === b.colors.length) {
+      switch (a.colors.length) {
+        case 0: return 0;
+        case 1: return a.colors[0] < b.colors[0] ? 1 : -1;
+        default:
+          forEach(a.colors, c => {
+            if (b.colors.includes(c)) {
+              b.colors.splice(b.colors.indexOf(c), 1);
+            }
+          });
+          if (b.colors.length > 0) {
+            // baaaahhh
+          } else {
+            return 0;
+          }
+      }
+      return 1;
+    } else {
+      return a.colors.length < b.colors.length ? 1 : -1;
+    }
+  }
   addDeck(e: any) {
     if (e && e.target && e.target.files[0]) {
       const fr = new FileReader();
